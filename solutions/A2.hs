4e818d9ed0dd82eacd3c5215bd26cfc5b0583a40
Hritik <hritik.s@greyorange.com>
2022-12-17 12:06:51 +0530

    assignment 2-1 completed

--- solutions/A2.hs
+++ solutions/A2.hs
@@ -9,40 +9,60 @@ import Data.List (intercalate)
 
 -- Q#01
 
-promptPlayer = undefined
+promptPlayer :: Player -> String
+promptPlayer p =
+    concat ["Player ",show p, "'s turn: enter a row and column position (ex. A1)"]
 
 -- Q#02
 
-_RANGE_ = undefined
+_RANGE_ :: [Int]
+_RANGE_ = [0 .. _SIZE_ - 1]
 
 -- Q#03
 
-isDigit = undefined
+isDigit :: Char -> Bool
+isDigit c =
+    c `elem` ['0' .. '9']
 
 
-readDigit = undefined
+readDigit :: Char -> Int
+readDigit d
+    | isDigit d = read [d]
+    | otherwise = -1
 
 -- Q#04
 
-_EMPTY_ROW_ = undefined
+_EMPTY_ROW_ :: [Square]
+_EMPTY_ROW_ = replicate _SIZE_ E
 
 
-_EMPTY_BOARD_ = undefined
+_EMPTY_BOARD_ :: [Row]
+_EMPTY_BOARD_ = replicate  _SIZE_ _EMPTY_ROW_
 
 -- Q#05
+-- Note: Actually you also should check that there is no win
+isTied :: Board -> Bool
+isTied b
+    | E `elem` concat b = False
+    | otherwise = True
 
-isTied = undefined
 
-
-_TIED_BOARD_ = undefined
+_TIED_BOARD_ :: Board
+_TIED_BOARD_ = [
+    [X, O, O]
+  , [O, X, X]
+  , [O, X, O]
+  ]
 
 -- Q#06
 
-indexRowStrings = undefined
+indexRowStrings :: [String] -> [(Char, String)]
+indexRowStrings ls = zip ['A' ..] ls 
 
 -- Q#07
 
-formatLine = undefined
+formatLine :: [String] -> String
+formatLine ls = concat [_SEP_, intercalate _SEP_ ls, _SEP_]
 
 -- *** Assignment 2-2 *** --
 
